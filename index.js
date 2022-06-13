2d28205bec8fa405bdba8ad2c1e51d5dbbe75445
kiran s <kirans08298@gmail.com>
2022-06-13 12:36:09 +0530

    third commit

--- index.js
+++ index.js
@@ -56,9 +56,10 @@ const server = app.listen(
 );
 
 const io = require("socket.io")(server, {
-  pingTimeout: 60000,
+  // pingTimeout: 60000,
   cors: {
-    origin: "https://random-chatapp.netlify.app/",
+    // origin: "https://random-chatapp.netlify.app/",
+    origin:"*"
     // credentials: true,
   },
 });
